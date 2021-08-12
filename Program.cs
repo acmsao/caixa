364e26e9557789730146e6cc671a3b2450592094
acmsao <69314905+acmsao@users.noreply.github.com>
2021-08-12 13:45:20 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using caixa.design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace caixa
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmVendas());
+
+            //frmLogin login = new frmLogin();
+            //login.ShowDialog();
+
+            //if (login.logado != null)
+            //{
+            //    Application.Run(new Principal(login.logado));
+            //}
+        }
+    }
+}
---
+++ dbCaixaDataSet.cs
@@ -0,0 +1,25 @@
+﻿namespace caixa
+{
+
+
+    partial class dbCaixaDataSet
+    {
+        partial class CaixaDataTable
+        {
+        }
+
+        partial class VendasDataTable
+        {
+        }
+    }
+}
+
+namespace caixa.dbCaixaDataSetTableAdapters
+{
+    partial class VendasTableAdapter
+    {
+    }
+
+    public partial class CaixaTableAdapter {
+    }
+}
